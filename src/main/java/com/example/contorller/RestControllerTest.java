d73f0480e7af4f1ff0f06a142a998ee3befb95ff
John Wong <john@JohndeMacBook-Pro.local>
2020-09-24 00:24:28 +0800

    feat:3

--- src/main/java/com/example/contorller/RestControllerTest.java
+++ src/main/java/com/example/contorller/RestControllerTest.java
@@ -1,14 +1,15 @@
 package com.example.contorller;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 //由Controller和ResponseBody组成，返回data 而不是 View
 public class RestControllerTest {
-    @RequestMapping("/go")
-    public String getGo(){
-        return "let's go";
+    @RequestMapping(value = "/journey/{id}")
+    public String getGo(@PathVariable("id") String id) {
+        return id;
     }
 
 
