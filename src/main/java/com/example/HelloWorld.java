c80a7eec0a110b43e62f98c43df48ea76d0379fb
John Wong <john@JohndeMacBook-Pro.local>
2020-09-23 20:25:16 +0800

    second commit

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -1,11 +1,12 @@
 package com.example;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 //注意启动类不能直接放在java目录下，要在一个包里
 public class HelloWorld {
     public static void main(String args[]){
-
+        SpringApplication.run(HelloWorld.class , args);
 
     }
 }
--- src/main/java/com/example/contorller/RestControllerTest.java
+++ src/main/java/com/example/contorller/RestControllerTest.java
@@ -1,6 +1,15 @@
 package com.example.contorller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
+//由Controller和ResponseBody组成，返回data 而不是 View
 public class RestControllerTest {
-    @RequestMapping
+    @RequestMapping("/go")
+    public String getGo(){
+        return "let's go";
+    }
+
+
 }
