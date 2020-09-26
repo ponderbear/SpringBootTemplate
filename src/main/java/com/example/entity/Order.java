50352888ada5188eb50f34c90f9bebfa91782593
John Wong <john@JohndeMacBook-Pro.local>
2020-09-26 17:01:34 +0800

    feat:date

---
+++ src/main/java/com/example/entity/Order.java
@@ -0,0 +1,69 @@
+package com.example.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+public class Order {
+    private String storeName;
+
+    //Spring默认使用Jackson的objectMapper自动处理字符串到date的映射，但只包括常见的pattern，如yyyy-MM-dd（传时的string到bean）
+    @JsonFormat(pattern = "yyyy-MM-dd")
+//    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date orderTime;
+
+    private String userId;
+
+    private String address;
+
+
+    public Order(String storeName, Date orderTime, String userId, String address) {
+        this.storeName = storeName;
+        this.orderTime = orderTime;
+        this.userId = userId;
+        this.address = address;
+    }
+
+    public String getStoreName() {
+        return storeName;
+    }
+
+    public void setStoreName(String storeName) {
+        this.storeName = storeName;
+    }
+
+    public Date getOrderTime() {
+        return orderTime;
+    }
+
+    public void setOrderTime(Date orderTime) {
+        this.orderTime = orderTime;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "storeName='" + storeName + '\'' +
+                ", orderTime=" + orderTime +
+                ", userId='" + userId + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
