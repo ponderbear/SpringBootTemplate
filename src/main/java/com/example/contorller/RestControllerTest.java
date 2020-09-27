8ecffdd05455b56a5cf490787c4ae7a702f5ac87
John Wong <zheaowang@163.com>
2020-09-27 08:54:18 +0800

    rD

--- src/main/java/com/example/contorller/RestControllerTest.java
+++ src/main/java/com/example/contorller/RestControllerTest.java
@@ -10,12 +10,16 @@ public class RestControllerTest {
 
     //value和path在源码层级互为引用，所以是等价的
     //@RequestMapping(value = "/{id}")
+
     @RequestMapping(path = "/{id}")
     public String getCertainOrder(@PathVariable("id") String id) {
         return id;
     }
-    //java8能根据url请求的路径名，名称的自动匹配
-    @RequestMapping(path = "/{orderId}/user/{userId}", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
+
+    //java 8能根据url请求的路径名，名称的自动匹配
+    //mime：mutipurpose internet mail extendsions；applicaionn :binary type/subType
+
+    @RequestMapping(path = "/{orderId}/user/{userId}", method = RequestMethod.POST, consumes = "application/json",produces = "application/json")
     @ResponseBody
     public User getOrderUser(@PathVariable String orderId, @PathVariable String userId){
         User oneUser = new User(orderId , userId);
